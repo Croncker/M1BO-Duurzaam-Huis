25531f95fae49cc1c20c30264501d9ee0eade6b2
Lucca <35749@ma-web.nl>
2023-04-04 11:21:01 +0200

    Bug gefixd

--- Dashboard/js/main.js
+++ Dashboard/js/main.js
@@ -26,7 +26,7 @@ document.getElementById("datum").innerHTML =  dag + " " + maand + "";
 setInterval(updateTime, 1000);
 
 
-//Weerbericht//
+// Weerbericht
 
 const apiKey = 'd103474f09b04cf7637f8989be03a3f7';
 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Amsterdam&units=metric&appid=${apiKey}`;
@@ -45,11 +45,8 @@ $(document).ready(() => {
   });
 });
 
-<<<<<<< HEAD
-//Zonsondegang//
+// Zonsondergang
 
-=======
->>>>>>> 140cbb6840db72655517b58fc8656d47796fb0a4
 const zonParagraaf = document.getElementById("zonParagraaf");
 const zonParagraaf2 = document.getElementById("zonParagraaf2");
 let data = fetch("https://api.sunrisesunset.io/json?lat=52.3910379&lng=4.8569872&timezone=UTC&date=today")
