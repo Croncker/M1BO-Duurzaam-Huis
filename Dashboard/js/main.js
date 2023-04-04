175a44fd410c9621411465a3ac6e7175d5de13fd
Lucca <35749@ma-web.nl>
2023-04-04 11:05:07 +0200

    update

--- Dashboard/js/main.js
+++ Dashboard/js/main.js
@@ -1,6 +1,6 @@
 function updateTime() {
     var now = new Date();
-    var hour = now.getUTCHours() + 2; // Horário de Amsterdam é UTC+2
+    var hour = now.getUTCHours() + 2;
     var minute = now.getUTCMinutes();
     var second = now.getUTCSeconds();
     document.getElementById('time').innerHTML = hour + ':' + minute + ':' + second;
@@ -9,13 +9,24 @@ function updateTime() {
     var day = days[now.getUTCDay()];
     document.getElementById('day').innerHTML = day;
 
-    var months = ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December'];
-    var month = months[now.getUTCMonth()];
-    document.getElementById('month').innerHTML = month;
+// Maak een nieuw date object aan
+var datum = new Date();
+
+// Haal de dag van de maand op
+var dag = datum.getDate();
+
+// Haal de maandnaam op
+var maanden = ["januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december"];
+var maand = maanden[datum.getMonth()];
+
+// Toon de datum op de pagina
+document.getElementById("datum").innerHTML =  dag + " " + maand + "";
 }
 
 setInterval(updateTime, 1000);
 
+
+//Weerbericht//
 
 const apiKey = 'd103474f09b04cf7637f8989be03a3f7';
 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Amsterdam&units=metric&appid=${apiKey}`;
@@ -29,7 +40,22 @@ $(document).ready(() => {
       $('.temperature').text(`${temperature}°C`);
     },
     error: () => {
-      $('.temperature').text('Erro ao buscar temperatura');
+      $('.temperature').text('Error');
     }
   });
 });
+
+//Zonsondegang//
+
+const zonParagraaf = document.getElementById("zonParagraaf");
+const zonParagraaf2 = document.getElementById("zonParagraaf2");
+let data = fetch("https://api.sunrisesunset.io/json?lat=52.3910379&lng=4.8569872&timezone=UTC&date=today")
+  .then(
+    function(binnengekomendata){
+        return binnengekomendata.json();
+    }).then(
+        function(echtedata){
+            zonParagraaf.innerText = echtedata.results.sunrise;
+            zonParagraaf2.innerText = echtedata.results.sunset;
+        }
+    );
